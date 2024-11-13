781b09add694e5f2dcafb821ca57e31b735b75bf
Ivan <ivantgam@gmail.com>
2024-11-13 15:03:24 +0300

    fix: attempts to fix pages

--- quasar.conf.js
+++ quasar.conf.js
@@ -37,7 +37,7 @@ module.exports = configure(function (ctx) {
     build: {
       env: require("dotenv").config().parsed,
       publicPath: process.env.NODE_ENV === 'open-dbdiagram' ? '/' : '/',
-      vueRouterMode: "history",
+      vueRouterMode: "hash",
       chainWebpack(/* chain */) {
       }
     },
